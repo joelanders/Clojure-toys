e1a7dc54ca851834e6fc3d037d70ed5e7cebac7f
Joe Landers <joe@joelanders.net>
2012-03-22 17:02:30 -0600

    More Clojurey. Still not quite beautiful.

--- hanoi.clj
+++ hanoi.clj
@@ -7,13 +7,16 @@
 (defn move-1 [[src dst oth]]
     [(rest src) (conj dst (first src)) oth])
 
-(defn move-n [[src dst oth] n]
+(defn swap [triple [new1st new2nd new3rd]]
+    [(triple new1st) (triple new2nd) (triple new3rd)])
+
+(defn move-n [towers n]
     (if (= 1 n)
-       (move-1 [src dst oth])
-       (let [[a c b] (move-n [src oth dst] (dec n))
-             [d e f] (move-1 [a b c])
-             [z y x] (move-n [f e d] (dec n))]
-           [x y z])))
+        (move-1 towers)
+        (-> towers
+            (swap [0 2 1]) (move-n (dec n)) (swap [0 2 1])
+            (move-1)
+            (swap [2 1 0]) (move-n (dec n)) (swap [2 1 0]))))
 
 (defn play [size]
     (move-n (make-board size) size))
