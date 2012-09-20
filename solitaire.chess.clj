5eee57815e15dee21a3c22b78a5ab8cc00621020
Joe Landers <joe@joelanders.net>
2012-09-20 02:00:43 -0600

    Pretty much working. Needs interface & clean-up.

--- solitaire.chess.clj
+++ solitaire.chess.clj
@@ -12,6 +12,14 @@
 (defn insert-piece [piece pos board]
   (assoc board pos piece))
 
+(defn remove-piece [pos board]
+  (insert-piece :o pos board))
+
+(defn move-piece [start end board]
+  (let [sym (board start)]
+    (insert-piece sym end
+                  (remove-piece start board))))
+
 ; pieces looks like '((:k [0 0]) (:q [0 1]))
 (defn populate-board [pieces board]
   (if (empty? pieces)
@@ -46,8 +54,18 @@
           (list (board [x y]) [x y])
           nil)))))
 
+; Returns the positions list.
+(defn occupied-positions [board]
+  (let [size (+ 1 (apply max (flatten (keys board))))]
+    (remove nil?
+      (for [x (range size), y (range size)]
+        (if (not= (board [x y]) :o)
+          [x y]
+          nil)))))
+
 ; eg. :o :k
 ;     :q :o
+
 (defn print-board [board]
   (let [size (+ 1 (apply max (flatten (keys board))))]
     (print
@@ -55,7 +73,8 @@
         (for [y (range size)]
           (join " "
             (map #(board [% y])
-                 (range size))))))))
+                 (range size)))))))
+  (println))
 
 (defn knight-moves [[x y] board]
   (remove nil?
@@ -102,7 +121,66 @@
               dy [(- 0 dx) dx] :when (not= dx dy 0)]
           [(+ dx x) (+ dy y)])))))
 
-(defn queen-moves [[x y] board]
-  (concat (rook-moves   [x y] board)
-          (bishop-moves [x y] board)))
+(defn queen-moves [pos board]
+  (concat (rook-moves   pos board)
+          (bishop-moves pos board)))
 
+;; Takes list of positions and a board,
+;; returns positions which are occupied.
+;; (no longer needed?)
+;(defn occupied [xys board]
+;  (remove nil?
+;    (map #(if (not= :o (board %)) % nil) xys)))
+
+; Takes a start position, a board, and a
+; list of end positions, returns a list
+; of boards with the piece at the start
+; position moved to each of the end positions.
+(defn next-boards [start ends board]
+  (map #(move-piece start % board) ends))
+
+; Takes a position and a board, returns
+; all of the boards that result from
+; the piece at that position making a
+; legal capture.
+(defn after-captures [start board]
+  (next-boards start
+               (captures start board)
+               board))
+
+
+; Takes the position of a piece, a board,
+; returns a list of positions which
+; the piece can capture.
+(defn captures [pos board]
+  (let [moves (case (board pos)
+                :p pawn-moves
+                :r rook-moves
+                :n knight-moves
+                :b bishop-moves
+                :q queen-moves
+                :k king-moves)]
+    (remove nil?
+      (map #(if (not= :o (board %)) % nil)
+          (moves pos board)))))
+
+; Takes a board and returns all of the
+; boards after any piece makes a capture.
+; So each of the resulting boards has
+; one fewer piece than the starting board.
+
+(defn after-all-captures [board]
+  (mapcat #(after-captures % board)
+       (occupied-positions board)))
+
+; Takes a list of boards and solves each
+; of them one step further.
+
+(defn step-list [boards]
+  (mapcat #(after-all-captures %) boards))
+
+; Print a list of boards
+
+(defn print-boards [boards]
+  (map #(do (print-board %) (println))
+       boards))
